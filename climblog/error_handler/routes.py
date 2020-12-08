f93abe7d44eac951795f11edb1b7fcab4501fae5
Harrison Wang <harrisonized@gmail.com>
2020-12-07 20:47:09 -0800

    hide traceback

--- climblog/error_handler/routes.py
+++ climblog/error_handler/routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template
 from werkzeug.debug import get_current_traceback
 import logging
+from apps.utils.config.settings import get_defaults_from_ini
+
+default_settings = get_defaults_from_ini()
+show_traceback = default_settings.getboolean('show_traceback')
 
 log = logging.getLogger(__name__)
 
@@ -11,25 +15,36 @@ error_handler = Blueprint('error_handler', __name__,
 
 @error_handler.app_errorhandler(404)
 def page_not_found(error):
-    error_message = get_current_traceback()
-    log.error(error_message.plaintext)
-    return render_template("404.html",
-                           error_message=error_message.plaintext), 404
+
+    if show_traceback:
+        error_message = get_current_traceback()
+        log.error(error_message.plaintext)
+        return render_template("404.html",
+                               error_message=error_message.plaintext), 404
+    
+    return render_template("404.html"), 404
+
 
 
 @error_handler.app_errorhandler(405)
 def method_not_allowed(error):
-    """Note: Blueprints use app_errorhandler instead of errorhandler"""
-    error_message = get_current_traceback()
-    log.error(error_message.plaintext)
-    return render_template("405.html",
-                           error_message=error_message.plaintext), 405
+
+    if show_traceback:
+        error_message = get_current_traceback()
+        log.error(error_message.plaintext)
+        return render_template("405.html",
+                               error_message=error_message.plaintext), 405
+
+    return render_template("405.html"), 405    
 
 
 @error_handler.app_errorhandler(500)
 def internal_server_error(error):
-    """Note: Blueprints use app_errorhandler instead of errorhandler"""
-    error_message = get_current_traceback()
-    log.error(error_message.plaintext)
-    return render_template("500.html",
-                           error_message=error_message.plaintext), 500
+    
+    if show_traceback:
+        error_message = get_current_traceback()
+        log.error(error_message.plaintext)
+        return render_template("500.html",
+                               error_message=error_message.plaintext), 500
+
+    return render_template("500.html"), 500
